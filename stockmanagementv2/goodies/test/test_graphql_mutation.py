b96a37b92ee7d906f23ffde6e542b70f83a00d90
smagucha <smagucha@gmail.com>
2022-05-22 09:40:22 +0300

    test graqhql nutattion

--- stockmanagementv2/goodies/test/test_graphql_mutation.py
+++ stockmanagementv2/goodies/test/test_graphql_mutation.py
@@ -3,34 +3,131 @@ import json
 from goodies.models import Product,Catergory
 
 class Test_Product_Catergory(GraphQLTestCase):
+	def setUp(self):
+		Catergory.objects.create(name='Electronic')
+		Product.objects.create(
+		name='sams',
+		productcatergory_id='1',
+		weight='7 kg',
+		quantity=100,
+	    )
 
 	def test_create_catergory(self):
 		response = self.query(
 			"""
-			mutation{
-  				createCatergory(name:"stationary"){
+			mutation($name: String!){
+  				createCatergory(name:$name){
     				name
 			  }
 			}
+			""",
+			variables = {"name": "stationary"}
+			)
+		
+		content = json.loads(response.content)
+		self.assertResponseNoErrors(response)
+	
+	def test_update_catergory(self):
+		response = self.query(
 			"""
+			mutation($id: Int!,  $name: String){
+				updateCatergory(id: $id, name: $name){
+		    		id,
+		    		name
+		 		}
+			}
+			""",
+			variables = {"id":1, "name":"Catergory"}
+		)
+		content = json.loads(response.content)
+		self.assertResponseNoErrors(response)
+		
+	def test_delete_catergory(self):
+		response = self.query(
+			"""
+			mutation($id: ID!){
+			 deleteCatergory(id: $id){
+			  catergory{
+			    id,
+			    name
+			  }
+			}
+			}
+			""",
+			variables ={"id":1}
 			)
 		content = json.loads(response.content)
 		self.assertResponseNoErrors(response)
-		print(response.content)
 
 	def test_create_product(self):
 		response = self.query(
 			"""
-			mutation{
-			  createProduct(name:"samas", productcatergoryId:3, quantity:200,weight:"1x9"){
+			mutation($name: String!, $productcatergoryId: Int!, $quantity: Int!, $weight: String!){
+			  createProduct(name: $name, productcatergoryId:$productcatergoryId, quantity: $quantity,weight:$weight){
 			    name,
 			    productcatergoryId,
 			    quantity,
 			    weight
 			  }
 			}
+			""",
+			variables = {"name": "samas", "productcatergoryId": 1,"quantity": 200, "weight":"1x9"}
+			)
+		content = json.loads(response.content)
+		self.assertResponseNoErrors(response)
+		
+
+	def test_update_product(self):
+		response = self.query(
 			"""
+			mutation($id:Int!, $name: String!, $productcatergoryId: Int!, $quantity: Int!, $weight: String!){
+				updateProduct(id:$id,name: $name, productcatergoryId:$productcatergoryId, quantity: $quantity,weight:$weight ){
+				  name,
+				  productcatergoryId,
+				  weight,
+				  quantity
+				}
+			}
+			""",
+			variables = {"id":1, "name": "samas", "productcatergoryId": 1,"quantity": 200, "weight":"1x9"}
+			)
+		content = json.loads(response.content)
+		self.assertResponseNoErrors(response)
+
+
+	def test_delete_product(self):
+		
+		response = self.query(
+			"""
+			mutation($id: ID!){
+			  deleteProduct(id: $id){
+			    product{
+			    	id,
+			    	name,
+			    }
+			  }
+			}
+			""",
+			variables={"id": 1}
+			)
+		content = json.loads(response.content)
+		self.assertResponseNoErrors(response)
+		
+
+	def test_add_quantity(self):
+		response = self.query(
+			"""
+			mutation($id: ID!, $quantity: Int!){
+			 addQuantity(id: $id,quantity: $quantity){
+			  quantity
+			}
+			}
+			""",
+			variables ={"id": 1,"quantity": 500}
 			)
 		content = json.loads(response.content)
 		self.assertResponseNoErrors(response)
 		print(response.content)
+		
+
+
